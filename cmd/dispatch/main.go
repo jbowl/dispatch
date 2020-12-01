b9ee85c6bf53442c1c232af0ea584c5803063b09
joel <joel.hanson@darkwolfsolutions.com>
2020-11-30 22:00:42 -0500

    Dockerfile

--- cmd/dispatch/main.go
+++ cmd/dispatch/main.go
@@ -1,17 +1,31 @@
+
+//https://www.openbrewerydb.org/documentation/01-listbreweries
+// https://stackoverflow.com/questions/56082458/grpc-organization-in-microservices
+// http://www.inanzzz.com/index.php/post/fczr/creating-a-simple-grpc-client-and-server-application-with-golang
+// https://github.com/public-apis/public-apis
+// https://stedolan.github.io/jq/tutorial/
+
+// docker build -f ./build/package/Dockerfile -t dispatch ../
+
+
 package main
 
 import (
 	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
-	"log"
+	"google.golang.org/grpc"
+	"io"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 	"os"
 	"os/signal"
 	"sync/atomic"
 	"time"
 	"github.com/gorilla/mux"
+
+	pb "github.com/jbowl/apibrewery"
+
 )
 
 type key int
@@ -23,6 +37,7 @@ const (
 var (
 	listenAddr string
 	healthy    int32
+    gTLS_bypass string
 )
 
 type dispatchServer struct {
@@ -31,12 +46,27 @@ type dispatchServer struct {
 	handler   http.Handler
 //	verifykey *rsa.PublicKey
 	healthy   int64
+
+	client pb.BreweryServiceClient
 }
+
+// respondWithDetails - writes a Content-Type:application/problem+json header
+//                  and a response body with details parameter
+func respondWithDetails(w http.ResponseWriter, details pb.ProblemDetails) {
+	w.Header().Set("Content-Type", "application/problem+json")
+	w.WriteHeader(details.Status)
+
+	json.NewEncoder(w).Encode(&details)
+}
+
+
 
 func (s *dispatchServer) NewRouter() *mux.Router {
 	r := mux.NewRouter().StrictSlash(true)
 
 	r.HandleFunc("/healthz", s.healthz).Methods("GET")
+	r.HandleFunc("/breweries", s.breweries).Methods("GET", "OPTIONS")
+	r.HandleFunc("/breweries/search", s.search).Methods("GET")
 
     return r
 }
@@ -68,13 +98,170 @@ func (s *dispatchServer) healthz(w http.ResponseWriter, r *http.Request) {
 }
 
 
+func (s *dispatchServer) search(w http.ResponseWriter, r *http.Request) {
+	//	keys := r.URL.Query()
+
+	//	city := keys.Get("by_city")
+
+	log.Printf("breweries")
+
+	filter := pb.Filter{By: r.URL.RawQuery}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	stream, err := s.client.SearchBreweries(ctx, &filter)
+	if err != nil {
+		log.Fatalf("%v.ListFeatures(_) = _, %v", s.client, err)
+	}
+	for {
+		brewery, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("%v.ListFeatures(_) = _, %v", s.client, err)
+		}
+		log.Printf("Brewery ID: %d name: %q website: %q", brewery.GetId(), brewery.GetName(), brewery.GetWebsiteUrl())
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(r.URL.RawQuery))
+}
+
+
+func enableCors(w *http.ResponseWriter, r *http.Request) {
+	origin := r.Header.Get("Origin")
+	// if !whitelisted return
+
+	(*w).Header().Set("Access-Control-Allow-Origin", origin)
+
+	//	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	//w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080/login")
+
+}
+
+func (s *dispatchServer) breweries(w http.ResponseWriter, r *http.Request) {
+//	keys := r.URL.Query()
+
+//	city := keys.Get("by_city")
+
+	if (*r).Method == "OPTIONS" {
+		enableCors(&w,r)
+		return
+	}
+
+
+	log.Printf("breweries")
+    log.Printf(r.URL.RawQuery)
+
+	filter := pb.Filter{By: r.URL.RawQuery}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	stream, err := s.client.ListBreweries(ctx, &filter)
+	if err != nil {
+			respondWithDetails(w, pb.ProblemDetails{
+				Detail:   err.Error(),
+				Type:     "",
+				Title:    http.StatusText(http.StatusInternalServerError),
+				Status:   http.StatusInternalServerError,
+				Instance: "",
+			})
+			//respondToClientWithError(w, err)
+			return
+	}
+
+
+
+	br := make([]pb.BreweryResult, 0)
+
+	for {
+		brewery, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("%v.ListFeatures(_) = _, %v", s.client, err)
+		}
+		br = append(br, pb.BreweryResult{
+			ID:      brewery.GetId(),
+			Name:    brewery.GetName(),
+			BreweryType: brewery.GetType(),
+			Street: brewery.GetStreet(),
+			City:    brewery.GetCity(),
+			State: brewery.GetState(),
+			CountryProvince: brewery.GetCountryprov(),
+			PostalCode: brewery.GetPostalcode(),
+			Country: brewery.GetCountry(),
+			Longitude: brewery.GetLongitude(),
+			Latitude: brewery.GetLatitude(),
+			Phone: brewery.GetPhone(),
+			Website: brewery.GetWebsiteUrl(),
+		})
+
+		//log.Printf("Brewery ID: %d name: %q website: %q", brewery.GetId(), brewery.GetName(), brewery.GetWebsiteUrl())
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&br)
+
+}
+
+func init() {
+
+	// log as JSON not default ASCII
+	log.SetFormatter(&log.JSONFormatter{})
+
+
+	/*
+		var file, err = os.OpenFile("./ptaas.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Println("Could Not Open Log File : " + err.Error())
+		}
+
+		// log to stdout not default stderr
+		log.SetOutput(file)
+	*/
+	log.SetOutput(os.Stdout)
+	// only log warning severity or above
+	//	log.SetLevel(log.WarnLevel)
+
+
+	log.Printf("init")
+
+}
+
 func run() error {
 
-	flag.StringVar(&listenAddr, "listen-addr", ":5000", "server listen address")
-	flag.Parse()
+	// runtime environment variables
+	listenPort := os.Getenv("PORT")
+	gTLS_bypass = os.Getenv("TLS_BYPASS")
 
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-	logger.Println("Server is starting...")
+	network :=  os.Getenv("NETWORK")
+
+	if  len(listenPort) == 0 || len(gTLS_bypass) == 0 || len(network) == 0{
+		log.WithFields(log.Fields{
+			"PORT":  listenPort,
+			"TLS_BYPASS": gTLS_bypass,
+			"NETWORK" : network,
+		}).Fatal("env arg missing")
+	}
+
+	log.WithFields(log.Fields{
+		"PORT":  listenPort,
+		"TLS_BYPASS": gTLS_bypass,
+		"NETWORK" : network,
+	}).Info("starting up with these settings")
+
+
+
+
+	//flag.StringVar(&listenAddr, "listen-addr", ":5000", "server listen address")
+	//flag.Parse()
+
+
+//	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+//	logger.Println("Server is starting...")
 
 	dispatch := dispatchServer{
 	}
@@ -94,34 +281,54 @@ func run() error {
 	// for healthz call
 	atomic.StoreInt64(&dispatch.healthy, time.Now().UnixNano())
 
+
+
+
+
 	httpServer := http.Server{
-		Addr:         ":" + "5000",
+		Addr:         ":" + listenPort,
 		ReadTimeout:  60 * time.Second,
 		WriteTimeout: 60 * time.Second,
 		IdleTimeout:  120 * time.Second,
-		Handler:      tracing(nextRequestID)(logging(logger)(dispatch.NewRouter())),
-
-//		Handler:      dispatch.NewRouter(),
+		Handler:      tracing(nextRequestID)(logging()(dispatch.NewRouter())),
 	}
 
 
-/*
-	server := &http.Server{
-		Addr:         listenAddr,
-		Handler:      tracing(nextRequestID)(logging(logger)(router)),
-		ErrorLog:     logger,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  15 * time.Second,
+	///////// grpc ->
+
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+
+//	opts = append(opts, grpc.WithBlock())
+
+	log.Printf("dialing")
+
+//	conn, err := grpc.Dial("localhost:50051", opts...)
+	//conn, err := grpc.Dial("breweryctr:50051", opts...)
+
+	log.Printf(network)
+
+	log.Printf(network + ":50051")
+
+	conn, err := grpc.Dial(network + ":50051", opts...)
+	if err != nil {
+		log.Printf("fail to dial: %v", err)
+	//	log.Fatalf("fail to dial: %v", err)
 	}
-*/
+	log.Printf("dialed")
+	defer conn.Close()
+	dispatch.client = pb.NewBreweryServiceClient(conn)
+
+
+	///////// <- grpc
+
 	done := make(chan bool)
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 
 	go func() {
 		<-quit
-		logger.Println("Server is shutting down...")
+		log.Println("Server is shutting down...")
 		atomic.StoreInt32(&healthy, 0)
 
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
@@ -129,19 +336,19 @@ func run() error {
 
 		httpServer.SetKeepAlivesEnabled(false)
 		if err := httpServer.Shutdown(ctx); err != nil {
-			logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+			log.Fatalf("Could not gracefully shutdown the server: %v\n", err)
 		}
 		close(done)
 	}()
 
-	logger.Println("Server is ready to handle requests at", listenAddr)
+	log.Println("Server is ready to handle requests at", listenAddr)
 	atomic.StoreInt32(&healthy, 1)
 	if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
+		log.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
 	}
 
 	<-done
-	logger.Println("Server stopped")
+	log.Println("Server stopped")
 
 	return nil
 }
@@ -181,15 +388,21 @@ func healthz() http.Handler {
 	})
 }
 
-func logging(logger *log.Logger) func(http.Handler) http.Handler {
+func logging() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			//setResponseHeaders(&w, r)
+			// needs to be removed and addressed via a whitelist lookup
+			enableCors(&w, r)
+
+
 			defer func() {
 				requestID, ok := r.Context().Value(requestIDKey).(string)
 				if !ok {
 					requestID = "unknown"
 				}
-				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+				log.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
 			}()
 			next.ServeHTTP(w, r)
 		})
--- go.mod
+++ go.mod
@@ -1,5 +1,16 @@
-module github.com/jbowl/locatebrewery/brewery
+module github.com/jbowl/dispatch
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jbowl/apibrewery v0.0.0-00010101000000-000000000000
+	github.com/sirupsen/logrus v1.7.0
+	google.golang.org/grpc v1.33.2
+
+)
+
+//replace github.com/jbowl/apibrewery => /home/j/jsoft/github.com/jbowl/apibrewery
+
+//replace github.com/jbowl/apibrewery => /home/j/jsoft/github.com/jbowl/dispatch/apibrewery
+replace github.com/jbowl/apibrewery => ./apibrewery
